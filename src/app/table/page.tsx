f52b8b5eecc46c328332a9b703d398a16ad961e5
AbdulHaseeb <abdullhaseebonline@gmail.com>
2024-07-07 13:40:24 +0500

    adding data

---
+++ src/app/table/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+type Props = {};
+
+const People = (props: Props) => {
+  return <Button>Click</Button>;
+};
+
+export default People;
